d2b52e3593c76d4d184416a22ddc1808f03f8f3f
twamorka <tanvi.wamorkar@cern.ch>
2022-08-24 19:59:00 -0500

    scripts to run MG jobs

--- make_MadGraph_script.py
+++ make_MadGraph_script.py
@@ -11,21 +11,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Produce a MadGraph script for scanning EFT parameter over the specified values for different processes.")
     parser.add_argument("process", type=str, action="store", help="The process to generate.")
     parser.add_argument("EFT_param", type=str, action="store", help="The EFT parameter to scan.")  
-    parser.add_argument("inclusive",type=bool,action="store",help="check if the process to be generated is inclusive or not")  
+    parser.add_argument("inclusive",type=str,action="store",help="check if the process to be generated is inclusive or not")  
     parser.add_argument("scan_values", nargs="+", help="Values to scan.")
-    parser.add_argument("out_dir",type="str",action="store",help="output dir name")
+    parser.add_argument("out_dir",type=str,action="store",help="output dir name")
 
     args = parser.parse_args()
 
-    out_dir = args.output if args.output else "."
+    out_dir = args.out_dir if args.out_dir else "."
     try:
         os.makedirs(out_dir)
     except FileExistsError:
         pass
-
+    print ("inclusive: ", args.inclusive)
     # with open(out_dir+"/mg_script_{}_{}.txt".format(args.process, args.EFT_param), "w") as f:
     with open(out_dir+"/mg_script_{}_{}.txt".format(args.process, args.EFT_param), "w") as f:
         f.write("""#########################################
+            {} (LO)
 #########################################
 
 
@@ -33,59 +34,59 @@ define p = g u c d s u~ c~ d~ s~ b b~
 define j = p
 define z1 = z
 define z2 = z
-""")
+""".format(args.EFT_param))
         model = ""
         generate_lines = ""
         if (args.EFT_param=="S0"):
             model = "import SM_LS012"
         if (args.process=="www"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > w+ w+ w- QCD=0
-                add process p p > w- w- w+ QCD=0"""
+add process p p > w- w- w+ QCD=0"""
             else:    
                 generate_lines = """generate p p > w+ w+ w- QCD=0, w+ > l+ vl, w- > j j
 add process p p > w- w- w+ QCD=0, w- > l- vl~, w+ > j j"""   
         elif (args.process=="wwz"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > w+ w- z """
             else:    
                 generate_lines  = """generate p p > w+ w- z, w+ > l+ vl, w- > l-  vl~, z > j j 
 add process p p > w+ w- z, w+ > l+ vl, w- > j j, z > l l
 add process p p > w+ w- z, w+ > j j, w- > l-, vl~, z > l l"""
         elif (args.process=="sswwjj"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > w+ w+ j j QCD=0
-                add process p p > w- w- j j QCD=0"""
+add process p p > w- w- j j QCD=0"""
             else:    
                 generate_lines = """generate p p > w+ w+ j j QCD=0, w+ > l+ vl
-            add process p p > w- w- j j QCD=0, w- > l- vl~"""
+add process p p > w- w- j j QCD=0, w- > l- vl~"""
         elif (args.process=="oswwjj"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > w+ w- j j QCD=0"""
             else:    
                 generate_lines = """generate p p > w+ w- j j QCD=0, w+ > l+ vl, w- > l- vl~"""
         elif (args.process=="wzjj"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > w+ z j j QCD=0
-                add process p p > w- z j j QCD=0"""
+add process p p > w- z j j QCD=0"""
             else:    
                 generate_lines = """generate p p > w+ z j j QCD=0, w+ > l+ vl, z > l+ l- 
-            add process p p > w- z j j QCD=0, w- > l- vl~, z > l+ l-"""
+add process p p > w- z j j QCD=0, w- > l- vl~, z > l+ l-"""
         elif (args.process=="zzjj"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > z z j j QCD=0"""
             else:    
                 generate_lines = """generate p p > z z j j QCD=0, z > l+ l-
-            generate p p > z1 z2 j j QCD=0, z1 > l l, z2 > j j"""    
+generate p p > z1 z2 j j QCD=0, z1 > l l, z2 > j j"""    
         elif (args.process=="wzz"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > w+ z z QCD=0
-                add process p p > w- z z QCD=0"""
+add process p p > w- z z QCD=0"""
             else:
                 generate_lines = """generate p p > w+ z1 z2 QCD=0, w+ > l+ vl, z1 > l+ l-, z2 > j j
-                add process p p > w- z1 z2 QCD=0, w- > l- vl~, z1 > l+ l-, z2 > j j"""    
+add process p p > w- z1 z2 QCD=0, w- > l- vl~, z1 > l+ l-, z2 > j j"""    
         elif (args.process=="zzz"):
-            if (args.inclusive==True):
+            if (args.inclusive=="1"):
                 generate_lines = """generate p p > z z z QCD=0"""
             else:
                 generate_lines = """generate p p > z1 z1 z2 QCD=0, z1 > l+ l-, z2 > j j """                
@@ -96,11 +97,11 @@ add process p p > w+ w- z, w+ > j j, w- > l-, vl~, z > l l"""
 output {}_aQGC_L{}
 launch {}_aQGC_L{}
 
-shower=PYTHIA8
-detector=PGS
-analysis=MADANALYSIS_5
-madspin=OFF
-reweight=OFF
+# shower=PYTHIA8
+# detector=PGS
+# analysis=MADANALYSIS_5
+# madspin=OFF
+# reweight=OFF
 done
 #——————————————————————————————————————
 ## Set Run card and Param card values:
@@ -142,7 +143,7 @@ set drbj 0.4
 set maxjetflavor 5
 #set event_norm sum
 
-done""".format(model, generate_lines, args.process, args.EFT_param, args.process, args.EFT_param, args.process, args.EFT_param))
+done""".format(model, generate_lines, args.out_dir, args.EFT_param, args.out_dir, args.EFT_param, args.out_dir, args.EFT_param))
 ## Loop over scan values
         f.write("""
 #########################################
@@ -152,13 +153,13 @@ done""".format(model, generate_lines, args.process, args.EFT_param, args.process
 done
 set run_tag {}_aQGC_F{}_{}
 set anoinputs 1 {}  #FS0
-done \n""".format(args.process, args.EFT_param, args.process, args.EFT_param, scan_value, scan_value))
+done \n""".format(args.out_dir, args.EFT_param, args.out_dir, args.EFT_param, scan_value, scan_value))
 
         f.write("""launch {}_aQGC_L{} -i 
 print_results --path=./cross_section_{}_aQGC_L{}.txt --format=short
 exit
 
-exit""".format(args.process, args.EFT_param, args.process, args.EFT_param))
+exit""".format(args.out_dir, args.EFT_param, args.out_dir, args.EFT_param))
 
 
        
--- run_MG_script.sh
+++ run_MG_script.sh
@@ -6,11 +6,22 @@ if [ $inclusive == 1 ]
 then
     fileExt="inclusive"
 fi
-process="sswwjj oswwjj wzjj zzjj www wwz wzz zzz";
-EFT_param="1e-20 1e-12 1e-11 1e-10 2e-10 3e-10 4e-10 1e-9 5e-9 1e-8"
+if [ $inclusive == 0 ] 
+then
+    fileExt="hadronic"
+fi
+# process="sswwjj oswwjj wzjj zzjj www wwz wzz zzz";
+process="sswwjj";
+
+# EFT_param="1e-20 1e-12 1e-11 1e-10 2e-10 3e-10 4e-10 1e-9 5e-9 1e-8"
+EFT_param="1e-11"
+echo $inclusive
 for p in $process;
 # for process in wzz;
 do 
-    echo python make_MadGraph_script.py ${p} S0 1 ${EFT_param} ${p}_${fileExt}
-    python make_MadGraph_script.py ${p} S0 1 ${EFT_param} ${p}_${fileExt}
+    echo python make_MadGraph_script.py ${p} S0 ${inclusive} ${EFT_param} ${p}_${fileExt}
+    python make_MadGraph_script.py ${p} S0 ${inclusive} ${EFT_param} ${p}_${fileExt}
+
+    # echo MG5_aMC_v3_4_0/bin/mg5_aMC ${p}_${fileExt}/mg_script_${p}_S0.txt
+    # MG5_aMC_v3_4_0/bin/mg5_aMC ${p}_${fileExt}/mg_script_${p}_S0.txt
 done
