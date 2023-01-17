3c69f4e36e760dc6b2c0dfd34dbeec3c28a226df
twamorka <tanvi.wamorkar@cern.ch>
2023-01-17 09:55:01 -0600

    script to create MG input

--- make_MadGraph_script.py
+++ make_MadGraph_script.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument("process", type=str, action="store", help="The process to generate.")
     parser.add_argument("EFT_param", type=str, action="store", help="The EFT parameter to scan.")  
     parser.add_argument("inclusive",type=str,action="store",help="check if the process to be generated is inclusive or not")  
-    parser.add_argument("scan_values", nargs="+", help="Values to scan.")
+    parser.add_argument("scan_values", type=float, action="store", help="Values to scan.")
     parser.add_argument("out_dir",type=str,action="store",help="output dir name")
 
     args = parser.parse_args()
@@ -37,8 +37,8 @@ define z2 = z
 """.format(args.EFT_param))
         model = ""
         generate_lines = ""
-        if (args.EFT_param=="S0"):
-            model = "import SM_Ltotal_Ind5v2020v2_UFO"
+        # if (args.EFT_param=="S0"):
+        #     model = "import SM_Ltotal_Ind5v2020v2_UFO"
         if (args.process=="www"):
             if (args.inclusive=="1"):
                 generate_lines = """generate p p > w+ w+ w- QCD=0
@@ -109,51 +109,52 @@ done
 
 ### param_card.dat:
 
-set anoinputs 1 1.000000e-20  #FS0
-set anoinputs 2 1.000000e-20  #FS1
-set anoinputs 3 1.000000e-20  #FS2
-set anoinputs 4 1.000000e-20  #FM0
-set anoinputs 5 1.000000e-20  #FM1
-set anoinputs 6 1.000000e-20  #FM2
-set anoinputs 7 1.000000e-20  #FM3
-set anoinputs 8 1.000000e-20  #FM4
-set anoinputs 9 1.000000e-20  #FM5
-set anoinputs 10 1.000000e-20  #FM6
-set anoinputs 11 1.000000e-20  #FM7
-set anoinputs 12 1.000000e-20  #FT0
-set anoinputs 13 1.000000e-20  #FT1
-set anoinputs 14 1.000000e-20  #FT2
-set anoinputs 15 1.000000e-20  #FT3
-set anoinputs 16 1.000000e-20  #FT4
-set anoinputs 17 1.000000e-20  #FT5
-set anoinputs 18 1.000000e-20  #FT6
-set anoinputs 19 1.000000e-20  #FT7
-set anoinputs 20 1.000000e-20  #FT8
-set anoinputs 21 1.000000e-20  #FT9
+set anoinputs 1 {}  #FS0
+set anoinputs 2 {}  #FS1
+set anoinputs 3 {}  #FS2
+set anoinputs 4 {}  #FM0
+set anoinputs 5 {}  #FM1
+set anoinputs 6 {}  #FM2
+set anoinputs 7 {}  #FM3
+set anoinputs 8 {}  #FM4
+set anoinputs 9 {}  #FM5
+set anoinputs 10 {}  #FM6
+set anoinputs 11 {}  #FM7
+set anoinputs 12 {}  #FT0
+set anoinputs 13 {}  #FT1
+set anoinputs 14 {}  #FT2
+set anoinputs 15 {}  #FT3
+set anoinputs 16 {}  #FT4
+set anoinputs 17 {}  #FT5
+set anoinputs 18 {}  #FT6
+set anoinputs 19 {}  #FT7
+set anoinputs 20 {}  #FT8
+set anoinputs 21 {}  #FT9
 
 ### run_card.dat:
 
-set run_tag {}_aQGC_F{}_1e-20_events
-set nevents 20000
+set run_tag {}_aQGC_F{}_{}_events
+set nevents 1000
 set bwcutoff 15.0
 set ptb 20.0
 set etab 5.0
 set drbb 0.4
 set drbj 0.4
 set maxjetflavor 5
+set iseed 0
 #set event_norm sum
 
-done""".format(model, generate_lines, args.out_dir, args.EFT_param, args.out_dir, args.EFT_param, args.out_dir, args.EFT_param))
-## Loop over scan values
-        f.write("""
-#########################################
-## Run through coupling parameters \n""")
-        for scan_value in args.scan_values:
-            f.write("""launch {}_aQGC_L{}
-done
-set run_tag {}_aQGC_F{}_{}
-set anoinputs 1 {}  #FS0
-done \n""".format(args.out_dir, args.EFT_param, args.out_dir, args.EFT_param, scan_value, scan_value))
+done""".format(model, generate_lines, args.out_dir, args.EFT_param, args.out_dir, args.EFT_param, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.scan_values, args.out_dir, args.EFT_param, args.scan_values))
+# ## Loop over scan values
+#         f.write("""
+# #########################################
+# ## Run through coupling parameters \n""")
+#         for scan_value in args.scan_values:
+#             f.write("""launch {}_aQGC_L{}
+# done
+# set run_tag {}_aQGC_F{}_{}
+# set anoinputs 1 {}  #FS0
+# done \n""".format(args.out_dir, args.EFT_param, args.out_dir, args.EFT_param, scan_value, scan_value))
 
         f.write("""launch {}_aQGC_L{} -i 
 print_results --path=./cross_section_{}_aQGC_L{}.txt --format=short
