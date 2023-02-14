59d56efe4d17e59d16cf43cd676ff6b90564af30
wamorkart <wamorkar.t@husky.neu.edu>
2023-02-14 17:11:23 +0100

    update for condor jobs

--- condor_submission_script.py
+++ condor_submission_script.py
@@ -29,22 +29,26 @@ queue arguments from arguments.txt
 
     script = '''#!/bin/sh -e
 PROCESS=$1;
-TYPE=$2;
-EFT_PARAM=$3;
-./mg5amcnlo/bin/mg5_aMC ${PROCESS}_${TYPE}/mg_script_${PROCESS}_${EFT_PARAM}.txt
+EFT_PARAM=$2;
+INCLUSIVE=$3;
+PARAM_VAL=$4;
+NEVENTS=$5;
+python2.7 /afs/cern.ch/work/t/twamorka/aQGC_Pheno/MG5_aMC_v2_9_9/bin/mg5_aMC /afs/cern.ch/work/t/twamorka/aQGC_Pheno/${PROCESS}_${EFT_PARAM}_${PARAM_VAL}_${INCLUSIVE}/mg_script_${PROCESS}_${EFT_PARAM}_${PARAM_VAL}.txt
 echo -e "DONE";
     '''
 
     arguments=[]
 
     process = ["sswwjj", "oswwjj", "wzjj", "zzjj", "www", "wwz", "wzz", "zzz"]
-    type = ["inclusive"]
-    eft_param = ["S0"]
-
-    for p in process:
-        for t in type:
-            for param in eft_param:
-                arguments.append("{} {} {}".format(p, t, param))
+    eft_params = ["S0"]
+    param_vals = [1e-20, 1e-12, 1e-11, 1e-10, 1e-9, 1e-8, 1e-7]
+    #param_vals = [1e-12, 2e-12, 4e-12, 6e-12, 8e-12, 1e-11, 2e-11, 4e-11, 6e-11, 8e-11, 1e-10, 2e-10, 4e-10, 6e-10, 8e-10, 1e-9, 2e-9, 4e-9, 6e-9, 8e-9, 1e-8, 2e-8, 4e-8, 6e-8, 8e-8, 1e-7, 2e-7, 4e-7, 6e-7, 8e-7, 1e-6 ]
+    inclusive = 1
+    nevents = 10000
+    for proc in process:
+        for param in eft_params:
+            for val in param_vals:
+                arguments.append("{} {} {} {} {}".format(proc, param, str(inclusive), val, nevents))
 
     with open("arguments.txt", "w") as args:
      args.write("\n".join(arguments))
--- run_MG_script.sh
+++ run_MG_script.sh
@@ -13,15 +13,18 @@ fi
 process="sswwjj oswwjj wzjj zzjj www wwz wzz zzz";
 # process="sswwjj";
 
-EFT_param="1e-20 1e-12 1e-11 1e-10 1e-9 5e-9 1e-8"
-# EFT_param="1e-11"
-echo $inclusive
+EFT_param="S0"
+value="1e-20 1e-12 1e-11 1e-10 1e-9 1e-8 1e-7"
+nevents=10000
 for p in $process;
 # for process in wzz;
-do 
-    echo python make_MadGraph_script.py ${p} S0 ${inclusive} ${EFT_param} ${p}_${fileExt}
-    python make_MadGraph_script.py ${p} S0 ${inclusive} ${EFT_param} ${p}_${fileExt}
-
+do
+    for v in $value;
+    do
+    
+       echo python3 make_MadGraph_script.py ${p} ${EFT_param} ${inclusive} ${v} ${nevents}
+       python3 make_MadGraph_script.py ${p} ${EFT_param} ${inclusive} ${v} ${nevents}
+    done
     # echo MG5_aMC_v3_4_0/bin/mg5_aMC ${p}_${fileExt}/mg_script_${p}_S0.txt
     # MG5_aMC_v3_4_0/bin/mg5_aMC ${p}_${fileExt}/mg_script_${p}_S0.txt
 done
